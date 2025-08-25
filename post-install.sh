ab2dfdfed359a9bbe703fdd2943c3cf73193bf98
Henrique Bissoli Malaman Alonso <84300368+henrique-1@users.noreply.github.com>
2025-08-24 22:15:45 -0300

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -461,8 +461,6 @@ EOSQL
 
 # 12. Deleta o Pod do MariaDB, se houver
 clean_mariadb_pod() {
-    print_header "Excluindo o Pod '$POD_NAME', se houver"
-
     local POD_NAME="mariadb-pod"
     local DB_VOLUME_NAME="mariadb_app_data"
     
